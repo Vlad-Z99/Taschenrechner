af9930ac13f54e4520082ccba4ad360e227510e5
Vladislav Zanfirov <126167687+Vlad-Z99@users.noreply.github.com>
2023-02-25 01:25:25 +0100

    Add files via upload

---
+++ Taschenrechner/Taschenrechner/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taschenrechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += (sender as Button).Text;
+        }
+
+        double a = 0, b = 0, c = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(textBox1.Text);
+            switch (mark)
+            {
+                case '+': c = a + b; 
+                    break;
+                case '-':
+                    c = a - b;
+                    break;
+                case 'x':
+                    c = a * b;
+                    break;
+                case '/':
+                    c = a / b;
+                    break;
+
+            }
+            textBox1.Text = c.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+        }
+
+        char mark = '+';
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            mark = (sender  as Button).Text[0];
+            textBox1.Clear();
+        }
+    }
+}
